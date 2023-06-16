ee7dae209d3b55b50ae62175500cceb3c2592fe8
Forrest Jones <forrestjones2010@gmail.com>
2023-06-16 14:33:52 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+  
+    var result = num1 + num2; // Modify this line for different operations
+  
+    document.getElementById('result').textContent = 'Result: ' + result;
+  }
+  
